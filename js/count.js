992778210bcde0c51d4aff98f49757a72183e894
VaneSkubic <vane.skubic@gmail.com>
2023-03-14 11:50:43 +0100

    Animacije za landing page

--- js/count.js
+++
@@ -1,5 +0,0 @@
-import { CountUp } from './countUp.min.js';
-const countUpStranke = new CountUp('stranke', 54, { enableScrollSpy: true, scrollSpyOnce: true });
-const countUpYears = new CountUp('leta', 7, { enableScrollSpy: true, scrollSpyOnce: true });
-countUpStranke.start();
-countUpYears.start();
