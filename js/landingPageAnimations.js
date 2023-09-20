dd8627854b400041639d45e0c901c3a83179a6e8
VaneSkubic <vane.skubic@gmail.com>
2023-09-20 14:54:07 +0200

    Content updates

--- js/landingPageAnimations.js
+++ js/landingPageAnimations.js
@@ -1,5 +1,5 @@
 import { CountUp } from './countUp.min.js';
-const countUpStranke = new CountUp('stranke', 54, { enableScrollSpy: true, scrollSpyOnce: true });
+const countUpStranke = new CountUp('stranke', 58, { enableScrollSpy: true, scrollSpyOnce: true });
 const countUpYears = new CountUp('leta', 7, { enableScrollSpy: true, scrollSpyOnce: true });
 countUpStranke.start();
 countUpYears.start();
